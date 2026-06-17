e121af2fa0ebf0ca0f3a7c8cc5cc67ad85fb921e
m4lu-ricard <malu.fernandes07@gmail.com>
2026-06-17 20:21:21 -0300

    Mexendo no banco

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,11 +1,12 @@
-// models/Usuario.js
 const mongoose = require('mongoose');
 
 const UsuarioSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     email: { type: String, required: true },
     usuario: { type: String, required: true, unique: true },
-    senha: { type: String, required: true }
+    senha: { type: String, required: true },
+    // Adicionamos a flag isAdmin. Por padrão, todo novo cadastro é um usuário comum.
+    isAdmin: { type: Boolean, default: false } 
 }, { timestamps: true }); 
 
 module.exports = mongoose.model('Usuario', UsuarioSchema);
